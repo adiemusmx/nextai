d7314acee4d37be8b80650744d6440a9d32b1807
adiemusmx <adiemusmx@users.noreply.github.com>
2015-04-22 00:02:45 +0800

    add food and move the snake

---
+++ src/define.h
@@ -0,0 +1,50 @@
+#include <GL/glut.h>
+
+// 位置的结构体
+struct stPosition
+{
+	GLint x;
+	GLint y;
+};
+
+// 记录食物的位置
+stPosition foodPosition;
+
+// 判断当前的游戏状态
+bool isRunning = false;
+
+// 游戏边界定义
+#define BORDER_UP		(50)
+#define BORDER_DOWN		(-50)
+#define BORDER_LEFT		(-50)
+#define BORDER_RIGHT	(50)
+
+#define SQUARE_LENGTH	(1)
+
+// 小蛇的身体初始化长度和最大长度
+#define SNAKE_INIT_LEN	(3)
+#define SNAKE_MAX_LEN	(200)
+
+int snakeLen = SNAKE_INIT_LEN;
+stPosition snakeBody[SNAKE_MAX_LEN]; 
+
+// 方向的枚举
+enum eDirection
+{
+	eDirectionUp,
+	eDirectionDown,
+	eDirectionLeft,
+	eDirectionRight
+};
+
+// 用来记录下一次小蛇向哪个方向进展
+eDirection nextDirection = eDirectionUp;
+
+// 用于避免小蛇在一次移动中接受到多个命令
+bool isValidSetDirection = true;
+
+bool isFood(GLint x, GLint y);
+stPosition randomFood(void);
+bool isFood(GLint x, GLint y);
+void eatFood(GLint x, GLint y);
+void initSnake();
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,13 @@
-#include <GL/glut.h>
-
-void initSnake();
+#include "define.h"
+#include <time.h>
+#include <stdlib.h>
 
 // 初始化OPENGL界面
 void initWindows(void)
 {
     glClearColor(0.0, 0.0, 0.0, 0.0);
     glMatrixMode(GL_PROJECTION);
-    glOrtho(-5.2, 5.3, -5.3, 5.2, 5, 15);
+    glOrtho(-52, 53, -53, 52, 5, 15);
     glMatrixMode(GL_MODELVIEW);
     gluLookAt(0, 0, 10, 0, 0, 0, 0, 1, 0);
 
@@ -18,15 +18,15 @@ void initWindows(void)
 }
 
 // 画正方形，用于显示方块。指定方块左上角的坐标和颜色
-void displaySquare(GLfloat x, GLfloat y, GLfloat red, GLfloat green, GLfloat blue)
+void displaySquare(GLint x, GLint y, GLint red, GLint green, GLint blue)
 {
 	glBegin(GL_QUADS);
 
 	glColor3f(red, green, blue);
 	glVertex2f(x,y);
-	glVertex2f(x + 0.1, y);
-	glVertex2f(x + 0.1, y - 0.1);
-	glVertex2f(x, y - 0.1);
+	glVertex2f(x + SQUARE_LENGTH, y);
+	glVertex2f(x + SQUARE_LENGTH, y - SQUARE_LENGTH);
+	glVertex2f(x, y - SQUARE_LENGTH);
 
 	glEnd();
 }
@@ -34,73 +34,82 @@ void displaySquare(GLfloat x, GLfloat y, GLfloat red, GLfloat green, GLfloat blu
 // 表示边框的函数
 void displayBorder(void)
 {
+	// 默认是红色的块
+	GLint red = 1;
+	GLint green = 0;
+	GLint blue = 0;
+
 	// 上
-	for (GLfloat x = -5; x <= 5; x = x + 0.1)
+	for (GLint x = BORDER_LEFT; x <= BORDER_RIGHT; x = x + SQUARE_LENGTH)
 	{
-		displaySquare(x, 5, 1.0f, 0.0f, 0.0f);
+		displaySquare(x, BORDER_UP, red, green, blue);
 	}
 
 	// 下
-	for (GLfloat x = -5; x <= 5; x = x + 0.1)
+	for (GLint x = BORDER_LEFT; x <= BORDER_RIGHT; x = x + SQUARE_LENGTH)
 	{
-		displaySquare(x, -5, 1.0f, 0.0f, 0.0f);
+		displaySquare(x, BORDER_DOWN, red, green, blue);
 	}
 
 	// 左
-	for (GLfloat y = -5; y <= 5; y = y + 0.1)
+	for (GLint y = BORDER_DOWN; y <= BORDER_UP; y = y + SQUARE_LENGTH)
 	{
-		displaySquare(5, y, 1.0f, 0.0f, 0.0f);
+		displaySquare(BORDER_LEFT, y, red, green, blue);
 	}
 
 	// 右
-	for (GLfloat y = -5; y <= 5; y = y + 0.1)
+	for (GLint y = BORDER_DOWN; y <= BORDER_UP; y = y + SQUARE_LENGTH)
 	{
-		displaySquare(-5, y, 1.0f, 0.0f, 0.0f);
+		displaySquare(BORDER_RIGHT, y, red, green, blue);
 	}
 }
 
-// 方向的枚举
-enum eDirection
+// 判断是否是边界
+bool isBorder(GLint x, GLint y)
 {
-	eDirectionUp,
-	eDirectionDown,
-	eDirectionLeft,
-	eDirectionRight
-};
-
-// 位置的结构体
-struct stPosition
-{
-	GLfloat x;
-	GLfloat y;
-};
-
-// 小蛇的身体
-#define SNAKE_INIT_LEN 3
-#define SNAKE_MAX_LEN 200
-int snakeMaxCount = SNAKE_INIT_LEN;
-stPosition snakeBody[SNAKE_MAX_LEN]; 
+	if (x <= BORDER_LEFT || x >= BORDER_RIGHT || y <= BORDER_DOWN || y >= BORDER_UP)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
 
 // 初始化小蛇
 void initSnake(void)
 {
-	snakeMaxCount = SNAKE_INIT_LEN;
+	isRunning = true;
+	foodPosition = randomFood();
+
+	snakeLen = SNAKE_INIT_LEN;
 	snakeBody[0].x = 0;
 	snakeBody[0].y = 0;
 	snakeBody[1].x = 0;
-	snakeBody[1].y = -0.1f;
+	snakeBody[1].y = -1 * SQUARE_LENGTH;
 	snakeBody[2].x = 0;
-	snakeBody[2].y = -0.2f;
+	snakeBody[2].y = -2 * SQUARE_LENGTH;
 }
 
-eDirection nextDirection = eDirectionUp;
-bool isValidSetDirection = true;
+// 是否是小蛇的范围
+bool isSnake(GLint x, GLint y)
+{
+	for (int i = 0; i < snakeLen; ++i)
+	{
+		if (snakeBody[i].x == x && snakeBody[i].y == y)
+		{
+			return true;
+		}
+	}
+	return false;
+}
 
 // 显示一条小蛇。指定小蛇的下一个位置
 void moveSnake(eDirection dir)
 {
-	GLfloat nextPositionX = 0.0f;
-	GLfloat nextPositionY = 0.0f;
+	GLint nextPositionX = snakeBody[0].x;
+	GLint nextPositionY = snakeBody[0].y;
 
 	isValidSetDirection = true;
 
@@ -108,39 +117,67 @@ void moveSnake(eDirection dir)
 	switch (dir)
 	{
 	case eDirectionUp:
-		nextPositionY = 0.1f;
+		nextPositionY += SQUARE_LENGTH;
 		break;
 	case eDirectionDown:
-		nextPositionY = -0.1f;
+		nextPositionY += -SQUARE_LENGTH;
 		break;
 	case eDirectionLeft:
-		nextPositionX = -0.1f;
+		nextPositionX += -SQUARE_LENGTH;
 		break;
 	case eDirectionRight:
-		nextPositionX = 0.1f;
+		nextPositionX += SQUARE_LENGTH;
 		break;
 	}
 
+	// 如果是食物，则吃掉
+	if (isFood(nextPositionX, nextPositionY))
+	{
+		eatFood(nextPositionX, nextPositionY);
+	}
+	// 如果是墙壁或者蛇本体，则推出
+	else if (isBorder(nextPositionX, nextPositionY) ||
+		isSnake(nextPositionX, nextPositionY))
+	{
+		isRunning = false;
+		return;
+	}
+
 	// 移动当前的数据
-	for (int i = snakeMaxCount - 1; i > 0; --i)
+	for (int i = snakeLen - 1; i > 0; --i)
 	{
 		snakeBody[i].x = snakeBody[i - 1].x;
 		snakeBody[i].y = snakeBody[i - 1].y;
 	}
 
 	// 设置头部的位置
-	snakeBody[0].x += nextPositionX;
-	snakeBody[0].y += nextPositionY;
+	snakeBody[0].x = nextPositionX;
+	snakeBody[0].y = nextPositionY;
 }
 
+// 表示小蛇的身体
 void displaySnake(void)
 {
-	for (int i = 0; i < snakeMaxCount; ++i)
+	// 默认是绿色的块
+	GLint red = 0;
+	GLint green = 1;
+	GLint blue = 0;
+	for (int i = 0; i < snakeLen; ++i)
 	{
-		displaySquare(snakeBody[i].x, snakeBody[i].y, 0.0f, 1.0f, 0.0f);
+		displaySquare(snakeBody[i].x, snakeBody[i].y, red, green, blue);
 	}
 }
 
+void displayFood(void)
+{
+	// 默认是绿色的块
+	GLint red = 0;
+	GLint green = 1;
+	GLint blue = 0;
+	displaySquare(foodPosition.x, foodPosition.y, red, green, blue);
+}
+
+// 显示主函数
 void displayFunc(void)
 {
     glClear(GL_COLOR_BUFFER_BIT);
@@ -151,6 +188,9 @@ void displayFunc(void)
 
 	// 显示一条小蛇
 	displaySnake();
+	
+	// 显示食物
+	displayFood();
 
 	// 交换两个缓存
 	glutSwapBuffers();
@@ -160,12 +200,13 @@ void displayFunc(void)
     return;
 }
 
+// 空闲控制函数
 void idleFunc(void)
 {
 	static int count = 0;
 	count++;
 
-	if (count > 20)
+	if (count > 5)
 	{
 		// 移动小蛇
 		moveSnake(nextDirection);
@@ -184,7 +225,7 @@ void keySetNextPosition(eDirection dir)
 		return;
 	}
 
-	// 小蛇不能因为键盘而反向运动
+	// 小蛇不能因为键盘而反向运动（有可能之后存在反向道具）
 	if ((dir == eDirectionUp && nextDirection == eDirectionDown) ||
 		(dir == eDirectionDown && nextDirection == eDirectionUp) ||
 		(dir == eDirectionLeft && nextDirection == eDirectionRight) ||
@@ -216,6 +257,67 @@ void keyBoardFunc(int key, int x, int y)
 	}
 }
 
+// 判断当前位置是不是食物
+bool isFood(GLint x, GLint y)
+{
+	if (x == foodPosition.x && y == foodPosition.y)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+// 随机一个食物的位置
+stPosition randomFood(void)
+{
+	stPosition randomPos;
+	while (true)
+	{
+		// 随机数，从边界到边界
+		randomPos.x = ((rand() % ((BORDER_RIGHT - BORDER_LEFT - 1) * 10) + 1) / 10.0f) + BORDER_LEFT;
+		randomPos.y = ((rand() % ((BORDER_UP - BORDER_DOWN - 1) * 10) + 1) / 10.0f) + BORDER_DOWN;
+
+		// 判断当前的位置是否存在食物，或者是否是蛇的身体
+		if (isFood(randomPos.x, randomPos.y) || isSnake(randomPos.x, randomPos.y))
+		{
+			continue;
+		}
+		else
+		{
+			break;
+		}
+	}
+	return randomPos;
+}
+
+// 吃掉一个食物
+void eatFood(GLint x, GLint y)
+{
+	// 判断当前的小蛇是否已经超过了最大的长度
+	if (snakeLen == SNAKE_MAX_LEN)
+	{
+		isRunning = false;
+	}
+	
+	++snakeLen;
+
+	// 移动当前的数据
+	for (int i = snakeLen - 1; i > 0; --i)
+	{
+		snakeBody[i].x = snakeBody[i - 1].x;
+		snakeBody[i].y = snakeBody[i - 1].y;
+	}
+
+	// 设置头部的位置
+	snakeBody[0].x = x;
+	snakeBody[0].y = y;
+
+	foodPosition = randomFood();
+}
+
 // 入场函数
 int main(int argc, char *argv[])
 {
@@ -232,7 +334,6 @@ int main(int argc, char *argv[])
 	// 描画
     glutDisplayFunc(displayFunc);
 	glutIdleFunc(idleFunc);
-	//glutKeyboardFunc(keyBoardFunc);
 	glutSpecialFunc(keyBoardFunc);
 
 	// 主循环
