7633927dfc02ab2a1ff744a9d00f493453ee06e9
adiemusmx <adiemusmx@aliyun.com>
2017-07-02 17:02:57 +0800

    add widget

--- include/widget_object_manager.h
+++ include/widget_object_manager.h
@@ -1,13 +1,18 @@
 #pragma once
 
+namespace Trinity
+{
 class ObjectManager
 {
 public:
 	static ObjectManager* getInstance();
+
+
 
 private:
 	ObjectManager();
 	~ObjectManager();
 
 	D_CLASS_DISABLE_COPY(ObjectManager);
+}
 }
