8e7c387b21a4b75e2f0b90df58ef77fd9e63ce28
adiemusmx <adiemusmx@aliyun.com>
2017-07-09 21:27:59 +0800

    update
    Signed-off-by: adiemusmx <adiemusmx@aliyun.com>

---
+++ include/widget_object.h
@@ -0,0 +1,15 @@
+#pragma once
+
+typedef int32 ObjectId;
+
+class WidgetObject
+{
+public:
+	WidgetObject();
+	~WidgetObject();
+	WidgetObject(const WidgetObject& obj);
+	WidgetObject& operator=(const WidgetObject& obj);
+
+private:
+
+}
